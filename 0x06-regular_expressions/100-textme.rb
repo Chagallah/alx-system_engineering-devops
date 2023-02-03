54b78b2240b21a81aa07612a59717255174a42f5
Chagallah <hchagalla@gmail.com>
2023-02-03 19:44:14 +0300

    correction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-
-puts ARGV[0].scan(/from:([+]?[[:alnum:]]*).*to:([+]?[[:alnum:]]*).*flags:((?:-?[0-9]:?){5})/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-
-puts ARGV[0].scan(/^h.{1}n$/).join
+puts ARGV[0].scan(/^h.n$/).join
